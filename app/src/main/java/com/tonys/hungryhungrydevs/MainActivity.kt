47a683b6e227e7f8d9dafb952500f3112c957f42
Minervus <tnguyen411@gmail.com>
2023-09-30 19:27:53 -0700

    updated textviews and added spacing

--- app/src/main/java/com/tonys/hungryhungrydevs/MainActivity.kt
+++ app/src/main/java/com/tonys/hungryhungrydevs/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+
+    CardView startersCard;
+    CardView mainsCard;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_mains);
     }
 }
